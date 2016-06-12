0d2e64ac52e0547c2031f7d2dda39d363858a62b
Stephen McCann <info@flatsteve.com>
2016-06-12 20:22:03 +0100

    Move state to top level component

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,30 @@
 'use strict';
 
 import React from 'react';
+import getBusTimes from '../services/tfl';
 import Departures from './departures/departures.js';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      busTimes: [],
+      stationName: 'Loading'
+    };
+  }
+
+  componentDidMount() {
+    this.requestBusTimeds = getBusTimes()
+      .then(function (data) {
+        data.sort((a, b) => { return a.timeToStation - b.timeToStation; });
+        this.setState({ busTimes: data, stationName: data[0].stationName });
+      }.bind(this));
+  }
+
   render() {
     return (
-      <Departures/>
+      <Departures busTimes={this.state.busTimes} stationName={this.state.stationName}/>
     );
   }
 }
--- app/components/departures/departures.js
+++ app/components/departures/departures.js
@@ -1,37 +1,19 @@
 'use strict';
 
 import React from 'react';
-import getBusTimes from '../../services/tfl';
 import DeparturesEntry from '../departure_entry/departures_entry';
 import './departures.css';
 
 class Departures extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      busTimes: [],
-      stationName: 'Loading'
-    };
-  }
-
-  componentDidMount() {
-    this.requestBusTimeds = getBusTimes()
-      .then(function (data) {
-        data.sort((a, b) => { return a.timeToStation - b.timeToStation; });
-        this.setState({ busTimes: data, stationName: data[0].stationName });
-      }.bind(this));
-  }
-
   render() {
     return (
       <div>
         <h2 className="departures__title">
-          Departures from {this.state.stationName}
+          Departures from {this.props.stationName}
         </h2>
 
         <div className="departures__board">
-          {this.state.busTimes.map(busDetails => {
+          {this.props.busTimes.map(busDetails => {
             return <DeparturesEntry details={busDetails} key={busDetails.id}/>;
           })}
         </div>
